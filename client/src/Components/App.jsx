9a82456a1875470177fb139eace027518e523cd2
fromhell13 <zalizan_zolkipali@astro.com.my>
2019-08-25 21:31:41 +0800

    styling footer

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { browserHistory } from 'react-router';
 import HomePage from './HomePage';
+import About from './About';
+import Works from './Works';
+import Resume from './Resume';
 import NavBar from './HeaderComponent/NavBar';
 import Footer from './FooterComponent/Footer';
 class App extends Component {
@@ -10,7 +13,12 @@ class App extends Component {
       <Router>
         <div>
           <NavBar />
+          <Switch>
           <Route name="home" exact path="/" component={HomePage} />
+          <Route name="about" path="/about" component={About} />
+          <Route name="works" path="/works" component={Works} />
+          <Route name="resume" path="/resume" component={Resume} />
+          </Switch>
           <Footer />
         </div>
       </Router>
--- client/src/Components/HeaderComponent/NavBar.jsx
+++ client/src/Components/HeaderComponent/NavBar.jsx
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+//import NavLink from './NavLink'
 class NavBar extends Component {
   render() {
     return (
-      <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to="">Home</Link></li>
-        </ul>
-      </header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+        <div class="container">
+          <span class="navbar-brand" href="#">
+            ZALIZAN ZOLKIPALI
+          </span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <Link to="/" class="nav-link">Home</Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="/about" class="nav-link">About</Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="/works" class="nav-link">Works</Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="/resume" class="nav-link">Resume</Link>
+                </li>
+              </ul>
+            </div>
+        </div>
+      </nav>
     )
   }
 }
 export default NavBar;
+
